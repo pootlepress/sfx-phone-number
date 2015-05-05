9be9209c1f72a0e0216346a54769e09f5e0bd594
Shramee <shramee.srivastav@gmail.com>
2015-05-05 23:01:46 +0530

    Improving SFXTP_Widget::widget()

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -50,19 +50,31 @@ class SFXTP_Widget extends WP_Widget {
 		//Adding the classes to before widget args
 		echo str_replace( 'widget_sfx-telephone', 'widget_sfx-telephone ' . $classes, $args['before_widget'] );
 
-		//The icon
-		$icon = ( 'none' == $i['fa-icon'] ) ? false : $i['fa-icon'];
-
-		//Mobile icon
-		if ( 'desktop' != $i['fa-icon-mobile'] ){
-			$icon_mob = ( 'none' == $i['fa-icon-mobile'] ) ? false : $i['fa-icon-mobile'];
-		} else {
-			$icon_mob = $icon;
-		}
-
 		/** @var string $id Caching the widget id */
 		$id = $this->id;
 
+		//Widget styles
+		$html = $this->widget_styles( $i, $id );
+
+		//Widget html
+		$html .= $this->widget_html( $i, $id, $classes );
+
+		//Print $html
+		echo $html;
+
+		//After widget args
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Returns the styles for the widget
+	 *
+	 * @param array $i instance
+	 * @param string $id the html id of the widget
+	 *
+	 * @return string Styles for the widget
+	 */
+	private function widget_styles( $i, $id ){
 		/** @var string $css The CSS for the widget */
 		$css = '';
 
@@ -83,30 +95,29 @@ class SFXTP_Widget extends WP_Widget {
 		$css .= "\t color: {$i['icon-hover-color']};\n";
 		$css .= "}\n";
 
-		/** @var string $html The HTML output for the widget */
-		$html = "<style>{$css}</style>";
+		return "<style>{$css}</style>";
+	}
 
-		if ( '' == $i['skype'] ) {
+	private function widget_html( $i, $id, $classes ){
+		if ( empty( $i['skype'] ) ) {
 			$classes .= 'sfxtp-phone-link ';
+			$url = 'callto://' . preg_replace( "/[^0-9]/", '',  $i['phone'] );
 			$span_text = $i['phone'];
 			$span_classes = 'sfxtp-phone';
-			$url = 'callto://' . preg_replace( "/[^0-9]/", '',  $i['phone'] );
 		} else {
 			$classes .= 'sfxtp-skype-link ';
+			$url = 'skype://' . preg_replace( "/[^.a-z\d]/i", '',  $i['skype'] );
 			$span_text = $i['skype'];
 			$span_classes = 'sfxtp-skype';
-			$url = 'skype://' . preg_replace( "/[^.a-z\d]/i", '',  $i['skype'] );
 		}
 
 		//Adding display format ( button / text ) class
 		$classes .= $i['display-format'];
 
 		//Adding anchor
-		$html .= "<a class='{$classes}' href='{$url}'>";
+		$html = "<a class='{$classes}' href='{$url}'>";
 
-		//Adding icon
-		$html .= $icon ? '<i class="icon fa ' . $icon . '"></i>' : '' ;
-		$html .= $icon_mob ? '<i class="mobile icon fa ' . $icon_mob . '"></i>' : '' ;
+		$html .= $this->widget_icon( $i, $id );
 
 		//Adding Call to action text
 		$html .= '<span class="sfxtp-cta-text">' . $i['cta-text'] . '</span>';
@@ -114,14 +125,64 @@ class SFXTP_Widget extends WP_Widget {
 		//Adding the contact
 		$html .= "<span class='{$span_classes}'> $span_text </span>";
 
+		return $html;
+	}
+
+	/**
+	 * Returns html for icon
+	 *
+	 * @param array $i instance
+	 *
+	 * @return string Styles for the widget
+	 */
+	private function widget_icon( $i ){
+
+		//The icon
+		$icon = $this->get_icon_class( $i['fa-icon'] );
+
+		//Mobile icon
+		if ( 'desktop' != $i['fa-icon-mobile'] ) {
+			$icon_mob = $this->get_icon_class( $i['fa-icon'] );
+		} else {
+			$icon_mob = $icon;
+		}
+
+		//Adding icon
+		$html = $this->get_icon_html( $icon, 'icon fa' );
+		$html .= $this->get_icon_html( $icon_mob, 'mobile icon fa' );
+
 		//Closing anchor
 		$html .= '</a>';
 
-		//Print $html
-		echo $html;
+		return $html;
+	}
 
-		//After widget args
-		echo $args['after_widget'];
+	/**
+	 * Returns the FA icon class
+	 * @param string $value
+	 * @return bool|string
+	 */
+	private function get_icon_class( $value ) {
+		if ( 'none' == $value ) {
+			$icon = false;
+		} else {
+			$icon = $value;
+		}
+
+		return $icon;
+	}
+
+	/**
+	 * @param string $icon_class
+	 * @param string $classes
+	 * @return string|bool HTML for icon output
+	 */
+	private function get_icon_html( $icon_class, $classes='' ) {
+		if ( $icon_class ) {
+			return "<i class=' $classes $icon_class '></i>";
+		}
+
+		return '';
 	}
 
 	/**
@@ -130,9 +191,10 @@ class SFXTP_Widget extends WP_Widget {
 	 * @see WP_Widget::form()
 	 *
 	 * @param array $instance Previously saved values from database.
+	 *
+	 * @return string|void
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'sfx-telephone' );
 		$fields = $this->fields();
 
 		foreach ( $fields as $f ){
